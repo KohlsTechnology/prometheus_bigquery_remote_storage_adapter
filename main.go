7b0aebc3cf33158dd03dcb1ce8cd289358c0aa9f
Heiko Zuerker <heiko.zuerker@kohls.com>
2020-03-23 08:59:30 -0500

    Update main.go

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ import (
 	"sync"
 	"time"
 
-	// "github.com/KohlsTechnology/prometheus_bigquery_remote_storage_adapter/bigquerydb"
+	"github.com/KohlsTechnology/prometheus_bigquery_remote_storage_adapter/bigquerydb"
 	"github.com/ReliabilityEngineering/prometheus_bigquery_remote_storage_adapter/bigquerydb"
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
